3daf84b7a482e9cb9706585aee326a1b463344d6
Fecovagyok <kistutu234@gmail.com>
2023-12-19 12:25:48 +0100

    Format day2 main.cc

--- day2/src/main.cc
+++ day2/src/main.cc
@@ -2,44 +2,45 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstdlib>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
 #define BUFF_SIZE 250
 
 #define COLOURS \
-  X(red) \
-  X(green) \
+  X(red)        \
+  X(green)      \
   X(blue)
 
-#define F_COMPARE(arg) else if(std::strcmp(stat_buf, #arg) == 0){ \
-  arg = current; \
-}
+#define F_COMPARE(arg)                         \
+  else if (std::strcmp(stat_buf, #arg) == 0) { \
+    arg = current;                             \
+  }
 
 struct Round {
   int64_t red = -1;
   int64_t green = -1;
   int64_t blue = -1;
 
-  uint16_t read_round(const std::string_view buf, std::string_view::const_iterator& it){
+  uint16_t read_round(const std::string_view buf,
+                      std::string_view::const_iterator& it) {
     char stat_buf[6];
-    while(it < buf.cend()){
-      if(*it == ';')
-        return 0;
-      if(*it == ','){
+    while (it < buf.cend()) {
+      if (*it == ';') return 0;
+      if (*it == ',') {
         it += 2;
         continue;
       }
       uint64_t current;
-      if(sscanf(it, "%lld %5[rbgendul]", &current, stat_buf) != 2){
+      if (sscanf(it, "%lld %5[rbgendul]", &current, stat_buf) != 2) {
         std::cerr << "Rossz input: " << buf << std::endl;
         exit(EXIT_FAILURE);
       }
-      #define X F_COMPARE
+#define X F_COMPARE
       COLOURS
-      #undef X
+#undef X
       else {
         std::cerr << "Rossz input: " << buf << std::endl;
         exit(EXIT_FAILURE);
@@ -50,9 +51,7 @@ struct Round {
     return 0;
   }
 
-  int64_t cube(){
-    return red*blue*green;
-  }
+  int64_t cube() { return red * blue * green; }
 };
 
 struct Game {
@@ -60,28 +59,26 @@ struct Game {
   std::vector<Round> rounds;
   Round max = {0, 0, 0};
 
-  #define F_STUFF(arg) \
-  for(Round round : rounds){ \
+#define F_STUFF(arg)                        \
+  for (Round round : rounds) {              \
     max.arg = std::max(max.arg, round.arg); \
   }
 
-  void process_rounds(){
+  void process_rounds() {
     F_STUFF(blue);
     F_STUFF(red);
     F_STUFF(green);
   }
 
-  Game() {
-    rounds.reserve(10);
-  }
-  size_t read_game(const std::string_view buf, std::string_view::const_iterator& it){
-    while(true){
+  Game() { rounds.reserve(10); }
+  size_t read_game(const std::string_view buf,
+                   std::string_view::const_iterator& it) {
+    while (true) {
       Round round;
       round.read_round(buf, it);
       rounds.push_back(round);
-      if(it >= buf.cend())
-        break;
-      if(*it != ';'){
+      if (it >= buf.cend()) break;
+      if (*it != ';') {
         std::printf("Rossz beolvasas: %s:%d\n", __func__, __LINE__);
         exit(EXIT_FAILURE);
       }
@@ -91,12 +88,11 @@ struct Game {
   }
 };
 
-
-uint64_t mr_logic(const std::string_view buf){
+uint64_t mr_logic(const std::string_view buf) {
   Game game;
   std::sscanf(buf.data(), "Game %lld: ", &(game.id));
   auto id = std::to_string(game.id);
-  size_t to_skip = sizeof("Game : ")-1+id.size();
+  size_t to_skip = sizeof("Game : ") - 1 + id.size();
   auto it = buf.cbegin() + to_skip;
   game.read_game(buf, it);
   game.process_rounds();
@@ -104,7 +100,7 @@ uint64_t mr_logic(const std::string_view buf){
   return game.max.cube();
 }
 
-int main(){
+int main() {
   std::ifstream input{"input.txt"};
   if (input.fail()) {
     std::cout << "Balfasz" << std::endl;
