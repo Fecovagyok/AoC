82d83ca380b5b4dce786c5a81c200f23c83a5c71
Fecovagyok <kistutu234@gmail.com>
2023-12-23 00:33:24 +0100

    day4/B ready

--- day4/src/day4.cc
+++ day4/src/day4.cc
@@ -6,6 +6,10 @@
 
 #include "aoc_reader.h"
 
+#define INPUT_SIZE 215
+#define WINNER_SIZE 10
+#define MY_SIZE 25
+
 uint16_t calcPoints(uint16_t count) {
   if (count == 0) return 0;
 
@@ -16,31 +20,45 @@ uint16_t calcPoints(uint16_t count) {
   return hatvany;
 }
 
+typedef std::vector<uint32_t> Card_t;
+
+uint32_t winCards(Card_t& card, uint16_t count, uint16_t cardNum) {
+  uint32_t cardQuota = card[cardNum - 1];
+  uint32_t ret_count = 0;
+  for (uint16_t i = cardNum; i < count + cardNum && i < card.size(); i++) {
+    card[i] += cardQuota;
+    ret_count += cardQuota;
+  }
+  return ret_count;
+}
+
 int main() {
   uint64_t points = 0;
-  auto process_line = [&points](std::string& buf) {
+  uint32_t cardNum = 1;
+  Card_t cards(INPUT_SIZE, 1);
+  auto process_line = [&](std::string& buf) {
     std::istringstream ss{buf};
     ss.ignore(buf.size(), ':');
     ss.ignore(buf.size(), ' ');
-    std::unordered_set<uint16_t> winners(10);
-    for (uint8_t i = 0; i < 10; i++) {
+    std::unordered_set<uint16_t> winners(WINNER_SIZE);
+    for (uint8_t i = 0; i < WINNER_SIZE; i++) {
       uint16_t in;
       ss >> in;
       winners.insert(in);
     }
     ss.ignore(3);
     uint16_t count = 0;
-    for (uint16_t i = 0; i < 25; i++) {
+    for (uint16_t i = 0; i < MY_SIZE; i++) {
       uint16_t in;
       ss >> in;
       if (winners.find(in) != winners.cend()) {
         count++;
       }
     }
-    points += calcPoints(count);
+    points += winCards(cards, count, cardNum);
+    cardNum++;
   };
-
   AoCReader reader{process_line, 270, "day4/input.txt"};
   reader.read();
-  std::cout << points << std::endl;
+  std::cout << points + INPUT_SIZE << std::endl;
 }
