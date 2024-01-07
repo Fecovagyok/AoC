474c01ed56bb60151c4c868e2bba938453a97b35
Fecovagyok <kistutu234@gmail.com>
2024-01-07 17:12:30 +0100

    should be good

--- day5/src/custom_tree.cc
+++ day5/src/custom_tree.cc
@@ -28,33 +28,20 @@ void recur_get_intervals(MyTreeMap* root, std::vector<Seed>& intervals,
     return;
   }
 
-  if (seed.seed_len == 0) return;
-
-  MyTreeEntry& entry = root->entry;
-  int64_t low_len = std::min(entry.src - seed.seed_start, seed.seed_len);
-  if (low_len > 0) {
-    recur_get_intervals(root->left, intervals,
-                        {seed.seed_start, (uint64_t)low_len});
-  }
-  uint64_t middle_start = low_len > 0 ? entry.src : entry.src - low_len;
-  int64_t middle_len =
-      seed.seed_start + seed.seed_len - (entry.src + entry.len);
-
-  if (middle_len > 0) {
-    intervals.push_back({middle_start, static_cast<uint64_t>(middle_len)});
-  }
+  const MyTreeEntry& entry = root->entry;
+  const uint64_t end = seed.seed_start + seed.seed_len;
+  const uint64_t entry_end = entry.src + entry.src;
 
   if (seed.seed_start < entry.src) {
-    if (seed.seed_start + seed.seed_len < entry.src) {
+    if (end < entry.src) {
       recur_get_intervals(root->left, intervals, seed);
       return;
     }
-    if (seed.seed_start + seed.seed_len < entry.src + entry.len) {
+    if (end <= entry_end) {
       recur_get_intervals(root->left, intervals,
                           {seed.seed_start, entry.src - seed.seed_start});
       // add seed entry.dest : entry.dest + seed_len
-      intervals.push_back(
-          {entry.dest, seed.seed_len - (entry.src - seed.seed_start)});
+      intervals.push_back({entry.dest, end - entry.src});
       return;
     }
     // seed.start + seed.len >= entry.src + entry.len
@@ -63,9 +50,24 @@ void recur_get_intervals(MyTreeMap* root, std::vector<Seed>& intervals,
     intervals.push_back({entry.dest, entry.len});
     recur_get_intervals(
         root->right, intervals,
-        {entry.src + entry.len,
-         seed.seed_len - (entry.src - seed.seed_start) - entry.len});
+        {entry_end, seed.seed_len - (entry.src - seed.seed_start) - entry.len});
+    return;
   }
+  if (seed.seed_start <= entry_end) {
+    const uint64_t start_offset = seed.seed_start - entry.src;
+    if (end <= entry_end) {
+      intervals.push_back({entry.dest + start_offset, seed.seed_len});
+      return;
+    }
+    // end > entry_end
+    intervals.push_back({entry.dest + start_offset, entry.len - start_offset});
+    recur_get_intervals(
+        root->right, intervals,
+        {entry_end, seed.seed_len - (entry.len - start_offset)});
+    return;
+  }
+  // seed_start > entry_end
+  recur_get_intervals(root->right, intervals, seed);
 }
 
 void MyTreeMap::insert(const MyTreeEntry& in_entry) {
