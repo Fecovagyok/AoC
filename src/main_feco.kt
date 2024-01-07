e46197f207b08949024facd3f63b357e07514d65
Fecovagyok <kistutu234@gmail.com>
2024-01-08 00:36:45 +0100

    func name renamed

--- src/main_feco.kt
+++ src/main_feco.kt
@@ -66,21 +66,21 @@ class MyTreeMap {
 
         if (seed.start < entry.src) {
             if (end < entry.src) {
-                recur_get_intervals(root->left, intervals, seed);
+                recurGetIntervals(root->left, intervals, seed);
                 return;
             }
             if (end <= entry_end) {
-                recur_get_intervals(root->left, intervals,
+                recurGetIntervals(root->left, intervals,
                 {seed.start, entry.src - seed.start});
                 // add seed entry.dest : entry.dest + seed_len
                 intervals.push_back({entry.dest, end - entry.src});
                 return;
             }
             // seed.start + seed.len >= entry.src + entry.len
-            recur_get_intervals(root->left, intervals,
+            recurGetIntervals(root->left, intervals,
             {seed.start, entry.src - seed.start});
             intervals.push_back({entry.dest, entry.len});
-            recur_get_intervals(
+            recurGetIntervals(
                 root->right, intervals,
             {entry_end, seed.len - (entry.src - seed.start) - entry.len});
             return;
@@ -93,12 +93,12 @@ class MyTreeMap {
             }
             // end > entry_end
             intervals.push_back({entry.dest + start_offset, entry.len - start_offset});
-            recur_get_intervals(root->right, intervals,
+            recurGetIntervals(root->right, intervals,
             {entry_end, seed.len - (entry.len - start_offset)});
             return;
         }
         // seed_start > entry_end
-        recur_get_intervals(root->right, intervals, seed);
+        recurGetIntervals(root->right, intervals, seed);
     }
 }
 
