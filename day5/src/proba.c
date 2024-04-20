8a17f97cbd45f2a992406c9c1524e9b98512c2d9
Fecovagyok <kistutu234@gmail.com>
2024-04-20 18:53:06 +0200

    Now it works, but it's seriously changed now

--- day5/src/proba.c
+++ day5/src/proba.c
@@ -4,30 +4,42 @@
 #include <stdlib.h>
 
 #define SEED 1
+#define DEFAULT_SIZE 16
+#define OP_SIZE DEFAULT_SIZE - 1
+#define NUM_OF_MISSING 4
+#define SHRANK_SIZE OP_SIZE - NUM_OF_MISSING
+#define NUM_OF_PAIRS DEFAULT_SIZE - NUM_OF_MISSING
+
+#define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
 
 typedef struct {
-  int array[16];
+  int array[OP_SIZE];
   uint16_t size;
 } Nums;
 
+typedef struct {
+  int num;
+  int next;
+} Pair;
+
 void init_nums(Nums* nums) {
-  for (uint16_t i = 0; i < 16; i++) {
-    nums->array[i] = i;
+  for (uint16_t i = 0; i < OP_SIZE; i++) {
+    nums->array[i] = i + 1;
   }
-  nums->size = 16;
+  nums->size = OP_SIZE;
 }
 
-bool is_among_missing(int missing[static 4], int num) {
-  for (uint16_t i = 0; i < 4; i++) {
+bool is_among_missing(int missing[static NUM_OF_MISSING], int num) {
+  for (uint16_t i = 0; i < NUM_OF_MISSING; i++) {
     if (num == missing[i]) return true;
   }
   return false;
 }
 
-void init_with_missing(Nums* nums, int missing[static 4]) {
-  nums->size = 12;
+void init_with_missing(Nums* nums, int missing[static NUM_OF_MISSING]) {
+  nums->size = SHRANK_SIZE;
   uint16_t i = 0;
-  for (int num = 0; num < 16; num++) {
+  for (int num = 1; num < OP_SIZE; num++) {
     if (is_among_missing(missing, num)) continue;
     nums->array[i] = num;
     i++;
@@ -57,24 +69,37 @@ int get_rand_next(Nums* nums) {
   return num;
 }
 
+void make_pairs(Pair pairs[static NUM_OF_PAIRS], Nums* available_nums) {
+  pairs[0].num = 0;
+  uint16_t i;
+  for (i = 0; i < SHRANK_SIZE; i++) {
+    int next_num = get_rand_next(available_nums);
+    pairs[i].next = next_num;
+    pairs[i + 1].num = next_num;
+  }
+  pairs[i].next = 0;
+}
+
 int main(void) {
   srand(SEED);
   Nums nums;
   init_nums(&nums);
-  int missing[4];
+  int missing[NUM_OF_MISSING];
 
   printf("Missing nums: ");
-  for (uint16_t i = 0; i < 4; i++) {
+  for (uint16_t i = 0; i < ARRAY_SIZE(missing); i++) {
     missing[i] = get_rand_next(&nums);
     printf("%d ", missing[i]);
   }
   printf("\n\n");
 
   init_with_missing(&nums, missing);
-  int excercise[12];
-  for (uint16_t i = 0; i < 12; i++) {
-    excercise[i] = get_rand_next(&nums);
-    printf("%d: %d\n", i, excercise[i]);
+
+  Pair pairs[NUM_OF_PAIRS];
+  make_pairs(pairs, &nums);
+
+  for (uint16_t i = 0; i < ARRAY_SIZE(pairs); i++) {
+    printf("%d:%d\n", pairs[i].num, pairs[i].next);
   }
 
   return 0;
