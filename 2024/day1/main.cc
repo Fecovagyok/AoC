edc69110c0407bcea859b2c153c49f5bf966a08d
Tumpek Ferenc Pál <kistutu234@gmail.com>
2024-12-02 17:51:25 +0100

    Start of day2

---
+++ 2024/day1/main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+int main(){
+  std::ios_base::sync_with_stdio(false);
+  std::ifstream{"2024/day1/input.txt"};
+
+  while(true){
+
+  }
+
+  return 0;
+}
+
