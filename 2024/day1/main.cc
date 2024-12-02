ba591ea701ada4e225de49db7deac2f3f1e8937d
Fecovagyok <kistutu234@gmail.com>
2024-12-02 18:14:03 +0100

    Not good but goes somewhere

--- 2024/day1/main.cc
+++ 2024/day1/main.cc
@@ -1,12 +1,40 @@
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 int main() {
   std::ios_base::sync_with_stdio(false);
-  std::ifstream input{"2024/day1/input.txt"};
-
-  while (true) {
+  std::ifstream input{"./2024/day1/input.txt"};
+  if (input.fail()) {
+    std::cerr << "F" << std::endl;
+    return 1;
   }
 
+  std::vector<uint32_t> left, right;
+  left.reserve(5000);
+  right.reserve(5000);
+
+  while (true) {
+    uint32_t tmp_left, tmp_right;
+    input >> tmp_left >> tmp_right;
+    if (input.eof() || input.fail()) break;
+    left.push_back(tmp_left);
+    right.push_back(tmp_right);
+  }
+  if (left.size() != right.size()) {
+    std::cerr << "Baj" << std::endl;
+    std::exit(1);
+  }
+  std::sort(left.begin(), left.end());
+  std::sort(right.begin(), right.end());
+
+  uint64_t sum = 0;
+  for (size_t i = 0; i < left.size(); i++) {
+    sum += std::abs(left[i] - right[i]);
+  }
   return 0;
 }
