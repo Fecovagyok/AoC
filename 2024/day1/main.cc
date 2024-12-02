588337e9ace4b1ea21f669ec05c75e170b05397d
Fecovagyok <kistutu234@gmail.com>
2024-12-02 21:32:50 +0100

    Part_two ready

--- 2024/day1/main.cc
+++ 2024/day1/main.cc
@@ -3,20 +3,13 @@
 #include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
-int main() {
-  std::ios_base::sync_with_stdio(false);
-  std::ifstream input{"./2024/day1/input.txt"};
-  if (input.fail()) {
-    std::cerr << "F" << std::endl;
-    return 1;
-  }
+using Dataset = std::vector<int32_t>;
 
-  std::vector<int32_t> left, right;
-  left.reserve(5000);
-  right.reserve(5000);
-
+void part_one(std::ifstream& input) {
+  Dataset left, right;
   while (true) {
     int32_t tmp_left, tmp_right;
     input >> tmp_left >> tmp_right;
@@ -24,6 +17,7 @@ int main() {
     left.push_back(tmp_left);
     right.push_back(tmp_right);
   }
+
   if (left.size() != right.size()) {
     std::cerr << "Baj" << std::endl;
     std::exit(1);
@@ -36,5 +30,43 @@ int main() {
     sum += std::abs(left[i] - right[i]);
   }
   std::cout << sum << std::endl;
+}
+
+void part_two(std::ifstream& input) {
+  std::unordered_map<uint32_t, uint32_t> right;
+  std::vector<uint32_t> left;
+  left.reserve(1000);
+  right.reserve(1000);
+
+  while (true) {
+    uint32_t tmp_left, right_key;
+    input >> tmp_left >> right_key;
+
+    if (input.eof()) break;
+
+    left.push_back(tmp_left);
+    if (right.find(right_key) == right.end()) {
+      right[right_key] = 1;
+    } else {
+      right[right_key]++;
+    }
+  }
+  uint32_t sum = 0;
+  for (size_t i = 0; i < left.size(); i++) {
+    if (right.find(left[i]) != right.end()) {
+      sum += left[i] * right[left[i]];
+    }
+  }
+  std::cout << sum << std::endl;
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::ifstream input{"./2024/day1/input.txt"};
+  if (input.fail()) {
+    std::cerr << "F" << std::endl;
+    return 1;
+  }
+  part_two(input);
   return 0;
 }
