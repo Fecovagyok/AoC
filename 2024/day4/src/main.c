4f2da4fcc808d498a85967d852d4bc3502656200
Fecovagyok <kistutu234@gmail.com>
2024-12-04 22:47:54 +0100

    I just realized I don't need this shit
    First padding then everythin else

---
+++ 2024/day4/src/main.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "matrix.h"
+
+int main(void) {
+  FILE* input = fopen("./2024/day4/input.txt", "rt");
+  if (input == NULL) {
+    fprintf(stderr, "Baj\n");
+    return 1;
+  }
+
+  read_matrix(input);
+  // uint32_t count = count_matrix(matrix);
+  // printf("%u\n", count);
+}
