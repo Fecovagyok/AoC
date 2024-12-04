51e9a8d0918a26da4e3ab77a7d899850a308006e
Fecovagyok <kistutu234@gmail.com>
2024-12-04 15:58:56 +0100

    Day3 part_one ready

---
+++ 2024/day3/src/day3.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+
+my $file_name = "$ARGV[0]/input.txt";
+open(my $input, "<", $file_name) or die "Could not open $!\n";
+
+my $sum = 0;
+while(<$input>){
+  while(/mul\((\d{1,3}),(\d{1,3})\)/g){
+    $sum += int($1) * int($2);
+  }
+  print "\n";
+}
+print $sum."\n";
