9f361f8c3b8ee4587b020264ec5d8f59b88a5392
Fecovagyok <kistutu234@gmail.com>
2024-12-04 21:58:00 +0100

    Day3 part_two not pretty but ready

--- 2024/day3/src/day3.pl
+++ 2024/day3/src/day3.pl
@@ -5,9 +5,33 @@ my $file_name = "$ARGV[0]/input.txt";
 open(my $input, "<", $file_name) or die "Could not open $!\n";
 
 my $sum = 0;
+my $state = 0;
 while(<$input>){
-  while(/mul\((\d{1,3}),(\d{1,3})\)/g){
-    $sum += int($1) * int($2);
+  while(/(.*?)mul\((\d{1,3}),(\d{1,3})\)/g){
+    my $rest = $1;
+    my $first = $2;
+    my $second = $3;
+    if($state == 0){
+      if($rest =~ /don't\(\).*?$/g){
+        if($rest =~ /do\(\).*?$/g){
+          print "Oh bizony van\n";
+          $sum += int($first) * int($second);
+        } else {
+          $state = 1;
+        }
+      } else {
+        $sum += int($first) * int($second);
+      }
+    } else {
+      if($rest =~ /do\(\).*?$/g){
+        if($rest =~ /don't\(\).*?$/g){
+          print "Oh bizony van\n";
+        } else {
+          $sum += int($first) * int($second);
+          $state = 0;
+        }
+      }
+    }
   }
 }
 print $sum."\n";
