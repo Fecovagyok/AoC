72fde6403ef64cb2a0c89e4e0e634487268062db
Fecovagyok <kistutu234@gmail.com>
2024-12-26 14:23:13 +0100

    Day9 part two ready

--- 2024/day9/src/diskin_stuff.cc
+++ 2024/day9/src/diskin_stuff.cc
@@ -1,52 +1,84 @@
 #include "diskin_stuff.h"
 
+#include <algorithm>
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <vector>
-
-static char slurped[20000 + 1];
-static uint64_t disk_map[sizeof(slurped) * 9];
-static size_t disk_count = 0;
 
 static const uint64_t FREE_SPACE_CHAR = UINT64_MAX;
 
-static std::vector<size_t> free_space_index;
+struct MyFile {
+  uint64_t id;
+  uint16_t size;
+  uint64_t start_idx;
+  bool is_free_space() const { return id == FREE_SPACE_CHAR; }
+};
+
+static char slurped[20000 + 1];
+std::list<MyFile> final_map;
+
+using MyIndex = std::list<MyFile>::iterator;
+static MyIndex disk_map[sizeof(slurped)];
+static size_t disk_count = 0;
+static std::vector<MyIndex> free_space_index;
 
 void slurp_it_whole(std::ifstream& input) {
-  free_space_index.reserve(1000);
+  free_space_index.reserve(5000);
   input.getline(slurped, sizeof(slurped));
+  uint64_t curr_idx = 0;
   for (size_t i = 0; slurped[i] != '\0'; i += 2) {
-    for (char j = 0; j < slurped[i] - '0'; j++) {
-      disk_map[disk_count] = i / 2;
-      disk_count++;
-    }
+    MyIndex file = final_map.insert(
+        final_map.end(),
+        MyFile{i / 2, static_cast<uint16_t>(slurped[i] - '0'), curr_idx});
+    disk_map[disk_count] = file;
+    curr_idx += file->size;
+    disk_count++;
     if (slurped[i + 1] == '\0') break;
-    for (char j = 0; j < slurped[i + 1] - '0'; j++) {
-      disk_map[disk_count] = FREE_SPACE_CHAR;
-      free_space_index.push_back(disk_count);
-      disk_count++;
-    }
+    file = final_map.insert(
+        final_map.end(),
+        MyFile{FREE_SPACE_CHAR, static_cast<uint16_t>(slurped[i + 1] - '0'),
+               curr_idx});
+    disk_map[disk_count] = file;
+    free_space_index.push_back(file);
+    curr_idx += file->size;
+    disk_count++;
   }
-  disk_map[disk_count] = 0;
 }
 
 void compact_da_disk() {
-  auto free_space_iter = free_space_index.begin();
   for (size_t i = 0; i < disk_count; i++) {
     size_t idx = disk_count - i - 1;
-    if (disk_map[idx] == FREE_SPACE_CHAR) continue;
-    if (free_space_iter == free_space_index.end() || *free_space_iter > idx)
-      break;
-    disk_map[*free_space_iter] = disk_map[idx];
-    ++free_space_iter;
-    disk_map[idx] = FREE_SPACE_CHAR;
+    MyIndex file = disk_map[idx];
+    if (file->is_free_space()) continue;
+    uint16_t size = file->size;
+    auto found_space = std::find_if(
+        free_space_index.begin(), free_space_index.end(),
+        [size](MyIndex free_size) { return free_size->size >= size; });
+    if (found_space == free_space_index.end()) continue;
+    MyIndex space_pos = *found_space;
+    if (space_pos->start_idx > file->start_idx) continue;
+    MyIndex inserted = final_map.insert(space_pos, *file);
+    file->id = FREE_SPACE_CHAR;
+    inserted->start_idx = space_pos->start_idx;
+    space_pos->size -= size;
+    space_pos->start_idx += size;
   }
   uint64_t checksum = 0;
-  for (size_t i = 0; i < disk_count; i++) {
-    if (disk_map[i] == FREE_SPACE_CHAR) break;
-    checksum += i * disk_map[i];
+  uint64_t position = 0;
+  for (const MyFile& file : final_map) {
+    if (file.is_free_space()) {
+      position += file.size;
+      continue;
+    }
+    assert(file.start_idx == position);
+    for (uint16_t i = 0; i < file.size; i++) {
+      checksum += (position + i) * file.id;
+    }
+    position += file.size;
   }
   std::cout << checksum << std::endl;
 }
