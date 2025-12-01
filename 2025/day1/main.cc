f28c3c87d0fc286fd6c7d6be7e85fc70ba85cf23
Fecovagyok <kistutu234@gmail.com>
2025-12-01 13:55:06 +0100

    Trying to overcomplicate it

--- 2025/day1/main.cc
+++ 2025/day1/main.cc
@@ -1,10 +1,11 @@
+#include <climits>
 #include <string>
 
 #include "aoc_reader.h"
 
 int main() {
-  int rot = 50;
-  int count = 0;
+  unsigned long rot = ((ULONG_MAX / 2) / 100) + 50;
+  unsigned long count = 0;
   auto cb = [&rot, &count](std::string& line) {
     if (line.length() < 2) {
       std::cerr << "WTF: " << line << std::endl;
@@ -12,7 +13,7 @@ int main() {
     }
     char rotDirection = line[0];
     const std::string substring = line.substr(1);
-    int val = std::stoi(substring);
+    unsigned long val = std::stoul(substring);
     if (rotDirection == 'L') {
       rot -= val;
     } else if (rotDirection == 'R') {
@@ -26,7 +27,7 @@ int main() {
       count++;
     }
   };
-  AoCReader aoc_reader{cb, 5, "2025/day1/input.txt"};
+  AoCReader aoc_reader{cb, 5, "2025/day1/input1.txt"};
   aoc_reader.read();
 
   std::cout << count << std::endl;
