7126dcaf836aebb05eab1398386aedf8324278c2
Tumpek Ferenc Pál <78788468+Fecovagyok@users.noreply.github.com>
2025-12-03 17:01:27 +0100

    Day3/2 example looks good

--- 2025/day3/main.cc
+++ 2025/day3/main.cc
@@ -1,28 +1,46 @@
 #include <aoc_reader.h>
 #include <cstdint>
 
-unsigned char process_line(std::string& line) {
-  using IType = std::string::size_type;
-  unsigned char max_begin = '0';
-  IType max_begin_index = 0;
-  for(IType i= 0; i < line.length() - 1; i++) {
-    unsigned char num = line[i];
-    if(num > max_begin) {
-      max_begin = num;
-      max_begin_index = i;
-    }
-  }
+using IType = std::string::size_type;
 
-  unsigned char max_end = '0';
-  for(IType i = max_begin_index + 1; i < line.length(); i++) {
-    unsigned char num = line[i];
-    if(num >= max_end) {
-      max_end = num;
+constexpr uint64_t powers_of_ten[] = {
+  1,
+  10,
+  100,
+  1000,
+  10000,
+  100000,
+  1000000,
+  10000000,
+  100000000,
+  1000000000,
+  10000000000,
+  100000000000,
+};
+
+void process_digit(std::string& line, IType pos, unsigned char max[], signed int max_index[]) {
+    for (IType i = max_index[pos - 1] + 1; i < line.length() - 11 + pos; i++) {
+      unsigned char num = line[i];
+      if(num > max[pos]) {
+        max[pos]         = num;
+        max_index[pos] = i;
+      }
     }
+}
+
+uint64_t process_line(std::string& line) {
+  unsigned char max_array[12] = {};
+  signed int max_index_array[13] = {};
+  max_index_array[0] = -1;
+
+  uint64_t result = 0;
+
+  for(IType i = 0; i < 12; i++) {
+    process_digit(line, i, max_array, max_index_array + 1);
+    result += static_cast<uint64_t>(max_array[i] - '0') * powers_of_ten[11 - i];
   }
 
   // unsigned char end_value[3] = {max_begin, max_end, '\0'};
-  unsigned char result = (max_begin - '0') * 10 + max_end - '0';
 
   return result;
 }
@@ -30,11 +48,10 @@ unsigned char process_line(std::string& line) {
 int main() {
   uint64_t sum = 0;
   auto cb = [&sum](std::string& line) {
-    unsigned char num = process_line(line);
-    uint64_t big_num = static_cast<uint64_t>(num);
+    uint64_t big_num = process_line(line);
     sum += big_num;
   };
-  AoCReader reader{cb, 1024, "input.txt"};
+  AoCReader reader{cb, 1024, "input1.txt"};
   reader.read();
   std::cout << sum << std::endl;
   return 0;
