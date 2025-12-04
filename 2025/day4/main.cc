bfc27f744e047bbedff55c3490dce66400f83de6
Tumpek Ferenc Pál <78788468+Fecovagyok@users.noreply.github.com>
2025-12-04 08:33:29 +0100

    Clangd format

--- 2025/day4/main.cc
+++ 2025/day4/main.cc
@@ -1,41 +1,40 @@
 #include <cstddef>
+
 #include "aoc_reader.h"
 #include "stuffed_matrix.h"
 
 using MyMatrix = StuffedMatrix<unsigned char, '\0', 1>;
 
 #define DIRECTIONS \
-  X(0, 1) \
-  X(0, -1) \
-  X(1, 0) \
-  X(1, 1) \
-  X(1, -1) \
-  X(-1, 0) \
-  X(-1, 1) \
+  X(0, 1)          \
+  X(0, -1)         \
+  X(1, 0)          \
+  X(1, 1)          \
+  X(1, -1)         \
+  X(-1, 0)         \
+  X(-1, 1)         \
   X(-1, -1)
-
 
 int how_many_adjacents(MyMatrix& matrix, size_t i, size_t j) {
   unsigned char value = matrix[i][j];
-  if(value != '@') {
+  if (value != '@') {
     return 0;
   }
 
-
   int ret = 0;
-  #define X(vertical, horizontal) \
-    if(matrix[i + (vertical)][j + (horizontal)] == '@') { \
-      ret++; \
-    }
+#define X(vertical, horizontal)                          \
+  if (matrix[i + (vertical)][j + (horizontal)] == '@') { \
+    ret++;                                               \
+  }
   DIRECTIONS
-  #undef X
+#undef X
   return ret;
 }
 
 void process_forklifts(MyMatrix& matrix) {
-  for(size_t i = 0; i < matrix.size(); i++) {
+  for (size_t i = 0; i < matrix.size(); i++) {
     auto row = matrix[i];
-    for(size_t j =0; j < row.size(); j++) {
+    for (size_t j = 0; j < row.size(); j++) {
       how_many_adjacents(matrix, i, j);
     }
   }
@@ -43,9 +42,7 @@ void process_forklifts(MyMatrix& matrix) {
 
 int main() {
   MyMatrix matrix;
-  auto cb = [&matrix](std::string& line) {
-    matrix.add_row(line);
-  };
+  auto cb = [&matrix](std::string& line) { matrix.add_row(line); };
   AoCReader reader{cb, 1024, "input1.txt"};
   reader.read();
   matrix.row_end();
