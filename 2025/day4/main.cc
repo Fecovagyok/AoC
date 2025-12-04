71db740e51d74bd9b022fbe3b29bfe4086f7584f
Tumpek Ferenc Pál <78788468+Fecovagyok@users.noreply.github.com>
2025-12-04 08:40:29 +0100

    Day4/1 ready

--- 2025/day4/main.cc
+++ 2025/day4/main.cc
@@ -16,11 +16,6 @@ using MyMatrix = StuffedMatrix<unsigned char, '\0', 1>;
   X(-1, -1)
 
 int how_many_adjacents(MyMatrix& matrix, size_t i, size_t j) {
-  unsigned char value = matrix[i][j];
-  if (value != '@') {
-    return 0;
-  }
-
   int ret = 0;
 #define X(vertical, horizontal)                          \
   if (matrix[i + (vertical)][j + (horizontal)] == '@') { \
@@ -31,20 +26,27 @@ int how_many_adjacents(MyMatrix& matrix, size_t i, size_t j) {
   return ret;
 }
 
-void process_forklifts(MyMatrix& matrix) {
+void process_rolls(MyMatrix& matrix) {
+  uint64_t count = 0;
   for (size_t i = 0; i < matrix.size(); i++) {
     auto row = matrix[i];
     for (size_t j = 0; j < row.size(); j++) {
-      how_many_adjacents(matrix, i, j);
+      if (row[j] != '@') continue;
+      int result = how_many_adjacents(matrix, i, j);
+      if (result < 4) {
+        count++;
+      }
     }
   }
+  std::cout << count << std::endl;
 }
 
 int main() {
   MyMatrix matrix;
   auto cb = [&matrix](std::string& line) { matrix.add_row(line); };
-  AoCReader reader{cb, 1024, "input1.txt"};
+  AoCReader reader{cb, 1024, "input.txt"};
   reader.read();
   matrix.row_end();
+  process_rolls(matrix);
   return 0;
 }
