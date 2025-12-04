c514b8170b5dc1465b80359bcd07eec91f2138ff
Tumpek Ferenc Pál <78788468+Fecovagyok@users.noreply.github.com>
2025-12-04 08:58:18 +0100

    day4/2 ready

--- 2025/day4/main.cc
+++ 2025/day4/main.cc
@@ -1,4 +1,6 @@
 #include <cstddef>
+#include <cstdint>
+#include <vector>
 
 #include "aoc_reader.h"
 #include "stuffed_matrix.h"
@@ -26,19 +28,40 @@ int how_many_adjacents(MyMatrix& matrix, size_t i, size_t j) {
   return ret;
 }
 
-void process_rolls(MyMatrix& matrix) {
+struct MatrixIndex {
+  size_t i;
+  size_t j;
+};
+
+uint64_t process_rolls(MyMatrix& matrix) {
   uint64_t count = 0;
+  static MatrixIndex scheduled_for_deletion[200 * 200];
+
   for (size_t i = 0; i < matrix.size(); i++) {
     auto row = matrix[i];
     for (size_t j = 0; j < row.size(); j++) {
       if (row[j] != '@') continue;
       int result = how_many_adjacents(matrix, i, j);
       if (result < 4) {
+        scheduled_for_deletion[count] = {i, j};
         count++;
       }
     }
   }
-  std::cout << count << std::endl;
+  for (size_t i = 0; i < count; i++) {
+    MatrixIndex idx = scheduled_for_deletion[i];
+    matrix[idx.i][idx.j] = '.';
+  }
+  return count;
+}
+
+void process_rolls_multiple_times(MyMatrix& matrix) {
+  uint64_t result_of_one;
+  uint64_t sum = 0;
+  while ((result_of_one = process_rolls(matrix)) != 0) {
+    sum += result_of_one;
+  }
+  std::cout << sum << std::endl;
 }
 
 int main() {
@@ -47,6 +70,6 @@ int main() {
   AoCReader reader{cb, 1024, "input.txt"};
   reader.read();
   matrix.row_end();
-  process_rolls(matrix);
+  process_rolls_multiple_times(matrix);
   return 0;
 }
