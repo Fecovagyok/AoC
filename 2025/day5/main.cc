8b9144c2496e25306f9384855f7758efa5eccf96
Fecovagyok <kistutu234@gmail.com>
2025-12-05 08:46:30 +0100

    Seems ready

--- 2025/day5/main.cc
+++ 2025/day5/main.cc
@@ -14,12 +14,18 @@ void add_range(uint64_t start, uint64_t end) {
   }
 }
 
+bool is_inside(uint64_t ing_id) {
+  auto found = ingr_map.find(ing_id);
+  return found != ingr_map.end();
+}
+
 int main() {
   bool ranges_reading = true;
   std::regex pattern{"(\\d+)-(\\d+)"};
   std::smatch match;
+  uint64_t count = 0;
 
-  auto cb = [&ranges_reading, &pattern, &match](std::string& line) {
+  auto cb = [&ranges_reading, &pattern, &match, &count](std::string& line) {
     if (line.empty()) {
       ranges_reading = false;
       std::cout << "\n";
@@ -39,15 +45,17 @@ int main() {
       static_assert(sizeof(unsigned long long) == 8);
       uint64_t start = std::stoull(match[1]);
       uint64_t end = std::stoull(match[2]);
+      add_range(start, end);
 
-      std::cout << start << "-" << end << "\n";
     } else {
       uint64_t ing_id = std::stoull(line);
-      std::cout << ing_id << "\n";
+      if (is_inside(ing_id)) {
+        count++;
+      }
     }
   };
   AoCReader reader{cb, 200, "2025/day5/input1.txt"};
   reader.read();
-  std::cout << std::endl;
+  std::cout << count << std::endl;
   return 0;
 }
