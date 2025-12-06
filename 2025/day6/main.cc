afa39381f91d7d5137f0fbf555466873ac1368b5
Fecovagyok <kistutu234@gmail.com>
2025-12-06 10:20:38 +0100

    Day6/1 Works with example

--- 2025/day6/main.cc
+++ 2025/day6/main.cc
@@ -1,8 +1,84 @@
 #include <aoc_reader.h>
 
+#include <array>
+#include <cctype>
+#include <cstdint>
+#include <sstream>
+#include <string>
+
+struct Operation {
+  uint64_t operand1;
+  uint64_t operand2;
+  uint64_t operand3;
+  enum class _operator { NOTHING, ADDITION, MULTIPLICATION };
+  _operator _operator;
+};
+
+using MyEverything = std::array<Operation, 4096>;
+
+void process_operand(MyEverything& everything, uint64_t Operation::* operand,
+                     std::string& line) {
+  std::istringstream is{line};
+  int count = 0;
+  while (!is.eof()) {
+    uint64_t num;
+    is >> num;
+    if (is.bad() || is.fail()) {
+      break;
+    }
+    everything[count++].*operand = num;
+  }
+}
+
+void process_operation(MyEverything& everything, std::string& line) {
+  using IType = std::string::size_type;
+  int count = 0;
+  for (IType i = 0; i < line.size(); i++) {
+    if (line[i] == '+') {
+      everything[count++]._operator = Operation::_operator::ADDITION;
+    }
+    if (line[i] == '*') {
+      everything[count++]._operator = Operation::_operator::MULTIPLICATION;
+    }
+  }
+}
+
+void process_all_operations(MyEverything& everything) {
+  uint64_t result = 0;
+  for (int i = 0; everything[i]._operator != Operation::_operator::NOTHING;
+       i++) {
+    if (everything[i]._operator == Operation::_operator::ADDITION) {
+      result += everything[i].operand1 + everything[i].operand2 +
+                everything[i].operand3;
+    } else {
+      result += everything[i].operand1 * everything[i].operand2 *
+                everything[i].operand3;
+    }
+  }
+  std::cout << result << std::endl;
+}
+
 int main() {
-  auto cb = [](std::string&) {};
-  AoCReader reader{cb, 8192, "2025/day6/input1.txt"};
+  MyEverything everything = {};
+  int cnt = 0;
+  auto cb = [&everything, &cnt](std::string& line) {
+    switch (cnt) {
+      case 0:
+        process_operand(everything, &Operation::operand1, line);
+        break;
+      case 1:
+        process_operand(everything, &Operation::operand2, line);
+        break;
+      case 2:
+        process_operand(everything, &Operation::operand3, line);
+      default:
+        process_operation(everything, line);
+        break;
+    }
+    cnt++;
+  };
+  AoCReader reader{cb, 4096, "2025/day6/input1.txt"};
   reader.read();
+  process_all_operations(everything);
   return 0;
 }
