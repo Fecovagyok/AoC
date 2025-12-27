5c642ef8169702209af557f08e780c030f38db90
Fecovagyok <kistutu234@gmail.com>
2025-12-27 18:48:59 +0100

    Revert "Delete 11/1"
    This reverts commit 7fb7d8c9a271d7717dd4e6de8399ec2583e19273.

--- 2025/day11/main.cc
+++ 2025/day11/main.cc
@@ -1,5 +1,13 @@
+#include <lemon/connectivity.h>
+#include <lemon/core.h>
+#include <lemon/list_graph.h>
+
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+
 #include "aoc_reader.h"
-#include "my_part2_helper.h"
+#include "universe.h"
 
 int main() {
   Universe universe;
@@ -7,5 +15,30 @@ int main() {
   AoCReader reader{cb, 512, "2025/day11/input2.txt"};
   reader.read();
   universe.createInverseMap();
-  kalap(universe);
+  lemon::ListDigraph::NodeMap<int> nodes_order{universe.graph};
+  if (!lemon::checkedTopologicalSort(universe.graph, nodes_order)) {
+    std::cerr << "Baj" << std::endl;
+  }
+
+  std::vector<lemon::ListDigraph::NodeIt> topologicalSorted(
+      universe.nodeMap.size(), lemon::INVALID);
+
+  for (lemon::ListDigraph::NodeIt i{universe.graph}; i != lemon::INVALID; ++i) {
+    size_t order = nodes_order[i];
+    topologicalSorted[order] = i;
+  }
+
+  lemon::ListDigraph::Node start = universe.nodeMap[START_NODE_STR];
+  lemon::ListDigraph::NodeMap<uint64_t> distinctPathMap(universe.graph, 0);
+  distinctPathMap[start] = 1;
+  for (auto current_vertex : topologicalSorted) {
+    for (lemon::ListDigraph::OutArcIt arc{universe.graph, current_vertex};
+         arc != lemon::INVALID; ++arc) {
+      auto other_end_vertex = universe.graph.target(arc);
+      distinctPathMap[other_end_vertex] += distinctPathMap[current_vertex];
+    }
+  }
+
+  lemon::ListDigraph::Node end = universe.nodeMap[END_NODE_STR];
+  std::cout << distinctPathMap[end] << std::endl;
 }
