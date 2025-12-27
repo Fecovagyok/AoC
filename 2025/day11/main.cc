0e0bf1f49a1158b2e085d3b701b2833bb3fd5f0b
Fecovagyok <kistutu234@gmail.com>
2025-12-27 19:35:09 +0100

    Day11/2 ready
    I did not need the rest of my experiment stuff
    I just needed to add some state to the algorythm.

--- 2025/day11/main.cc
+++ 2025/day11/main.cc
@@ -2,6 +2,7 @@
 #include <lemon/core.h>
 #include <lemon/list_graph.h>
 
+#include <array>
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
@@ -12,7 +13,7 @@
 int main() {
   Universe universe;
   auto cb = [&universe](std::string& line) { universe.parse(line); };
-  AoCReader reader{cb, 512, "2025/day11/input2.txt"};
+  AoCReader reader{cb, 512, "2025/day11/input.txt"};
   reader.read();
   universe.createInverseMap();
   lemon::ListDigraph::NodeMap<int> nodes_order{universe.graph};
@@ -29,16 +30,40 @@ int main() {
   }
 
   lemon::ListDigraph::Node start = universe.nodeMap[START_NODE_STR];
-  lemon::ListDigraph::NodeMap<uint64_t> distinctPathMap(universe.graph, 0);
-  distinctPathMap[start] = 1;
+  lemon::ListDigraph::NodeMap<std::array<uint64_t, 4>> distinctPathMap(
+      universe.graph, {0, 0, 0, 0});
+
+  const lemon::ListDigraph::Node dac_node = universe.nodeMap["dac"];
+  const lemon::ListDigraph::Node fft_node = universe.nodeMap["fft"];
+  enum TraverseState : size_t {
+    SEEN_NOTHING = 0,
+    SEEN_DAC = 1,
+    SEEN_FFT = 2,
+    SEEN_BOTH = SEEN_DAC | SEEN_FFT,
+    TRAVERSE_STATE_MAX = 4
+  };
+
+  distinctPathMap[start][0] = 1;
   for (auto current_vertex : topologicalSorted) {
-    for (lemon::ListDigraph::OutArcIt arc{universe.graph, current_vertex};
-         arc != lemon::INVALID; ++arc) {
-      auto other_end_vertex = universe.graph.target(arc);
-      distinctPathMap[other_end_vertex] += distinctPathMap[current_vertex];
+    for (size_t i = 0; i < TRAVERSE_STATE_MAX; i++) {
+      uint64_t been_reached_this_way = distinctPathMap[current_vertex][i];
+      if (been_reached_this_way == 0) continue;
+      for (lemon::ListDigraph::OutArcIt arc{universe.graph, current_vertex};
+           arc != lemon::INVALID; ++arc) {
+        auto other_end_vertex = universe.graph.target(arc);
+        size_t other_vertex_state = i;
+        if (other_end_vertex == dac_node) {
+          other_vertex_state |= SEEN_DAC;
+        }
+        if (other_end_vertex == fft_node) {
+          other_vertex_state |= SEEN_FFT;
+        }
+        distinctPathMap[other_end_vertex][other_vertex_state] +=
+            been_reached_this_way;
+      }
     }
   }
 
   lemon::ListDigraph::Node end = universe.nodeMap[END_NODE_STR];
-  std::cout << distinctPathMap[end] << std::endl;
+  std::cout << distinctPathMap[end][SEEN_BOTH] << std::endl;
 }
--- 2025/day11/my_part2_helper.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "universe.h"
-
-void kalap(Universe& universe);
