221432d4e9ca9fd2de72587ccd9979c7c3bae16e
Fecovagyok <kistutu234@gmail.com>
2026-01-04 11:55:46 +0100

    Renamed stuff

--- 2025/day9/main.cc
+++ 2025/day9/main.cc
@@ -23,12 +23,12 @@ struct Coord {
     return coord[idx];
   }
 
-  bool operator==(const Coord other) {
+  bool operator==(const Coord other) const {
     return other.coord[0] == coord[0] && other.coord[1] == coord[1];
   }
 };
 
-struct Rectangle {
+struct Tile {
 #define DIRECTIONS \
   X(Up, -1, 0)     \
   X(Down, 1, 0)    \
@@ -42,9 +42,9 @@ struct Rectangle {
   Coord coord;
   Direction dir = Direction::Up;
   Direction prev_dir = Direction::Up;
-  Rectangle() {}
-  Rectangle(int32_t column, int32_t row) : coord{column, row} {}
-  uint64_t calc_area(Rectangle other) const {
+  Tile() {}
+  Tile(int32_t column, int32_t row) : coord{column, row} {}
+  uint64_t calc_area(const Tile other) const {
     uint64_t operand1 = std::abs(other.coord[0] - coord[0]) + 1;
     uint64_t operand2 = std::abs(other.coord[1] - coord[1]) + 1;
     return operand1 * operand2;
@@ -63,8 +63,8 @@ struct Rectangle {
 
   void move_forward() { move(dir); }
   void move_backward() { move(prev_dir); }
-  uint64_t operator*(Rectangle other) const { return calc_area(other); }
-  bool operator==(const Rectangle other) const { return other.coord == coord; }
+  uint64_t operator*(const Tile other) const { return calc_area(other); }
+  bool operator==(const Tile other) const { return other.coord == coord; }
 };
 
 namespace std {
@@ -78,41 +78,18 @@ struct hash<Coord> {
 }  // namespace std
 
 struct Universe {
-  std::vector<Rectangle> rectangles;
-  std::unordered_map<Coord, Rectangle> map;
+  std::vector<Tile> tiles;
+  std::unordered_map<Coord, Tile> map;
 
-  void fillShape() {
-    Rectangle f_current = rectangles[0];
-    Rectangle b_current = rectangles[0];
-    f_current.move_forward();
-    b_current.move_backward();
-    while (f_current != rectangles[0] || b_current != rectangles[0]) {
-      auto f_current_found = map.find(f_current.coord);
-      while (f_current_found == map.end() ||
-             f_current_found->second.dir == f_current.dir) {
-        f_current.move_forward();
-      }
-
-      auto b_current_found = map.find(b_current.coord);
-      while (b_current_found == map.end() ||
-             b_current_found->second.prev_dir == b_current.prev_dir) {
-        b_current.move_backward();
-      }
-      // Rectangle& prev =
-      //     i == 0 ? rectangles[rectangles.size() - 1] : rectangles[i - 1];
-      rectangles[i].dir
-    }
-  }
-
-  void outlineShape(Rectangle prev, Rectangle current) {
+  void outlineShape(Tile prev, Tile current) {
     int different_axis;
     if (prev.coord[0] == current.coord[0]) {
       different_axis = 1;
     } else {
       different_axis = 0;
     }
-    const Rectangle* bigger;
-    const Rectangle* smaller;
+    const Tile* bigger;
+    const Tile* smaller;
     if (current.coord[different_axis] > prev.coord[different_axis]) {
       bigger = &current;
       smaller = &prev;
@@ -122,7 +99,7 @@ struct Universe {
     }
     for (int32_t i = smaller->coord[different_axis];
          i <= bigger->coord[different_axis]; i++) {
-      Rectangle rectangle = *smaller;
+      Tile rectangle = *smaller;
       rectangle.coord[different_axis] = i;
       map.emplace(rectangle);
     }
@@ -131,23 +108,20 @@ struct Universe {
   void parse(std::string& line) {
     std::vector<std::string_view> pieces = split(line, ',');
     assert(pieces.size() == 2);
-    Rectangle current{static_cast<int32_t>(str_to_int32(pieces[0])),
-                      static_cast<int32_t>(str_to_int32(pieces[1]))};
-    // if (rectangles.size() > 0) {
-    //   Rectangle& prev = rectangles[rectangles.size() - 1];
-    //   outlineShape(prev, current);
-    // }
-    rectangles.push_back(current);
+    Tile current{static_cast<int32_t>(str_to_int32(pieces[0])),
+                 static_cast<int32_t>(str_to_int32(pieces[1]))};
+    if (tiles.size() > 0) {
+      Tile& prev = tiles[tiles.size() - 1];
+      outlineShape(prev, current);
+    }
+    tiles.push_back(current);
   }
 
-  void finishOutlineShape() {
-    outlineShape(rectangles[rectangles.size() - 1], rectangles[0]);
-  }
+  void finishOutlineShape() { outlineShape(tiles[tiles.size() - 1], tiles[0]); }
 
-  bool isValidRectangleOnAxis(const Rectangle rec1, const Rectangle rec2,
-                              int axis) {
-    const Rectangle* bigger;
-    const Rectangle* smaller;
+  bool isValidRectangleOnAxis(const Tile rec1, const Tile rec2, int axis) {
+    const Tile* bigger;
+    const Tile* smaller;
     if (rec1.coord[axis] > rec2.coord[axis]) {
       bigger = &rec1;
       smaller = &rec2;
@@ -157,7 +131,7 @@ struct Universe {
     }
 
     for (int32_t i = smaller->coord[axis]; i <= bigger->coord[axis]; i++) {
-      Rectangle tangle = *smaller;
+      Tile tangle = *smaller;
       tangle.coord[axis] = i;
       if (map.contains(tangle.coord)) {
         return false;
@@ -171,12 +145,12 @@ struct Universe {
     return true;
   }
 
-  bool isValidRectangle(const Rectangle rec1, const Rectangle rec2) {
-    if (!isValidRectangleOnAxis(rec1, rec2, 0)) {
+  bool isValidRectangle(const Tile tile1, const Tile tile2) {
+    if (!isValidRectangleOnAxis(tile1, tile2, 0)) {
       return false;
     }
 
-    if (!isValidRectangleOnAxis(rec1, rec2, 1)) {
+    if (!isValidRectangleOnAxis(tile1, tile2, 1)) {
       return false;
     }
     return true;
@@ -184,10 +158,10 @@ struct Universe {
 
   void solve() {
     uint64_t max = 0;
-    for (size_t i = 0; i < rectangles.size(); i++) {
-      Rectangle& one = rectangles[i];
-      for (size_t j = i + 1; j < rectangles.size(); j++) {
-        Rectangle& other = rectangles[j];
+    for (size_t i = 0; i < tiles.size(); i++) {
+      Tile& one = tiles[i];
+      for (size_t j = i + 1; j < tiles.size(); j++) {
+        Tile& other = tiles[j];
         if (!isValidRectangle(one, other)) {
           continue;
         }
@@ -203,7 +177,7 @@ struct Universe {
   void printTiles() {
     for (int32_t i = 0; i < 16; i++) {
       for (int32_t j = 0; j < 16; j++) {
-        Rectangle tangle{j, i};
+        Tile tangle{j, i};
         if (map.find(tangle) == map.end()) {
           std::cout << '.';
         } else {
@@ -217,7 +191,7 @@ struct Universe {
 
 int main() {
   Universe universe;
-  universe.rectangles.reserve(500);
+  universe.tiles.reserve(500);
   auto cb = [&universe](std::string& line) { universe.parse(line); };
   AoCReader reader{cb, 128, "2025/day9/input1.txt"};
   reader.read();
